4aaec804f8cf6a321c21c3b93038a8a2c5d0e518
Konrad <00ksiuzdak00@gmail.com>
2020-10-17 18:58:56 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ReadingsComponent } from './readings/readings.component';
+import { SensorsComponent } from './sensors/sensors.component';
+import { UploadComponent } from './upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: AppComponent
+  },
+  {
+    path: 'upload',
+    component: UploadComponent
+
+  },
+  {
+    path: 'sensors',
+    component: SensorsComponent
+  },
+  {
+    path: 'readings',
+    component: ReadingsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UploadComponent } from './upload/upload.component';
+import { SensorComponent } from './sensor/sensor.component';
+import { SensorsComponent } from './sensors/sensors.component';
+import { FormsModule } from '@angular/forms';
+import { ReadingsComponent } from './readings/readings.component';
+import { ReadingComponent } from './reading/reading.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UploadComponent,
+    SensorComponent,
+    SensorsComponent,
+    ReadingsComponent,
+    ReadingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
